7aee827224358672243ef9f3a77e5c7264e9d633
Sullivan12138 <44030194+Sullivan12138@users.noreply.github.com>
2019-05-30 16:53:31 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,33 @@
+int i,j;
+struct A
+{
+	int h;
+	char j;
+	float k;
+};
+int sad[20];
+	int fun(int a, float b)
+	//this is a line of comment
+	{
+	int m;
+	char c;
+	float x = 5.1;
+	/*
+	these are
+	three lines
+	of comments
+	*/
+	int happy[20];
+	sad[19] = happy[0] = 0;
+	
+	a = !b;
+	if (a>b) m=a;
+	else m=b;
+	printf("happ");
+	fun(1,2);
+	return m;
+	while(1) a = 5;
+	for (a = 0; a < 100; a++) a--;
+	}
+	float x,y;
+	char c;
